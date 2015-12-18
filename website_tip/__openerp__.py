9a18ca8d3832b3e8e42037b6c171e5ab2b41857b
Nedas Zilinskas <nedas.zilinskas@gmail.com>
2015-12-18 13:42:18 +0200

    .

--- website_tip/__openerp__.py
+++ website_tip/__openerp__.py
@@ -1,20 +1,27 @@
+# -*- coding: utf-8 -*-
+# © 2015 Nedas Žilinskas <nedas.zilinskas@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
-    'name': 'Tip',
-    'category': 'Website',
-    'summary': 'Add beautiful mouse-over tips on links',
-    'version': '8.0.2.0',
-    'description': """
+    "name": "Tip",
+    "category": "Website",
+    "summary": "Add beautiful mouse-over tips on links",
+    "version": "8.0.2.0",
+    "description": """
 Tip
 ======================================
 Add beautiful mouse-over tips on links.
 """,
-    'author': 'Nedas Žilinskas <nedas.zilinskas@gmail.com>',
-    'website': 'http://nedaszilinskas.com',
-    'depends': [
-        'website'
+    "author": "Nedas Žilinskas <nedas.zilinskas@gmail.com>",
+    "website": "http://nedaszilinskas.com",
+    "depends": [
+        "website"
     ],
-    'data': [
-        'views/assets.xml'
+    "data": [
+        "views/assets.xml"
     ],
-    'installable': True
+    "images": [
+        "static/description/main_screenshot.png",
+    ],
+    "installable": True
 }
